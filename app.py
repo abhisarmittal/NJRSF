b0061d605dc955e61bdf61a35d1e7c584234c745
yuvanshu <yuvanshu.agarwal21@gmail.com>
2019-02-26 22:33:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ from urllib.error import HTTPError
 
 from builtins import str
 from builtins import bytes
-from future import standard_library
-standard_library.install_aliases()
 from builtins import object
+
 import requests as rq
 import base64
 import pprint
